a854f2ae8a353217881ec89927b94491149b3301
n7commanderjohn <kimgears2@gmail.com>
2021-01-19 17:26:24 -0500

    installed packages and styles and added components

--- wiki-finder-spa/src/app/app.module.ts
+++ wiki-finder-spa/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBoxComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule,
